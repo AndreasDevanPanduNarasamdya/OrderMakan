536a54ca99edcd556ccba40b7d2148449bc3fc5d
AndreasDevanPanduNarasamdya <andreasdevan123@gmail.com>
2025-09-28 02:02:51 +0700

    yes

--- Food.java
+++ Food.java
@@ -7,15 +7,13 @@ public class Food implements Discountable {
         this.price = price;
     }
 
-    // Getters & Setters
     public String getName() { return name; }
     public double getPrice() { return price; }
     public void setName(String name) { this.name = name; }
     public void setPrice(double price) { this.price = price; }
 
-    // Implement discount (default 0%)
     @Override
     public double applyDiscount(double price) {
-        return price; // no discount by default
+        return price;
     }
 }
